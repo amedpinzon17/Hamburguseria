dec172ee388903f3e1a39844b23b17fca6902c5d
amedpinzon17 <pinzonf359@gmail.com>
2023-09-15 08:34:47 -0500

    feat:

--- README.md
+++ README.md
@@ -166,3 +166,285 @@ A continuación, se describen los endpoints disponibles en la aplicación junto 
 - Funciones nativas utilizadas:
   - `MongoClient`, `connect()`, `db.collection()`, `find()`, `sort()`, `toArray()`, `res.json()`
 
+## Endpoint 21
+- **URL:** `/endpoint21`
+- **Descripción:** Busca y devuelve ingredientes cuyo precio esté en el rango de $2 a $5.
+- **Método HTTP:** GET
+- **Funciones nativas utilizadas:**
+  - `MongoClient`, `connect()`, `db.collection()`, `find()`, `toArray()`, `res.json()`
+
+## Endpoint 22
+- **URL:** `/endpoint22`
+- **Descripción:** Actualiza la descripción del ingrediente "Pan" a "Pan fresco y crujiente".
+- **Método HTTP:** GET
+- **Funciones nativas utilizadas:**
+  - `MongoClient`, `connect()`, `db.collection()`, `updateOne()`, `res.json()`
+
+## Endpoint 23
+- **URL:** `/endpoint23`
+- **Descripción:** Busca hamburguesas que contengan los ingredientes "Tomate" y "Lechuga".
+- **Método HTTP:** GET
+- **Funciones nativas utilizadas:**
+  - `MongoClient`, `connect()`, `db.collection()`, `find()`, `toArray()`, `res.json()`
+
+## Endpoint 24
+- **URL:** `/endpoint24`
+- **Descripción:** Filtra chefs cuyo nombre no sea "ChefA".
+- **Método HTTP:** GET
+- **Funciones nativas utilizadas:**
+  - `MongoClient`, `connect()`, `db.collection()`, `find()`, `toArray()`, `res.json()`
+
+## Endpoint 25
+- **URL:** `/endpoint25`
+- **Descripción:** Incrementa el precio de las hamburguesas de la categoría "Gourmet" en $2.
+- **Método HTTP:** GET
+- **Funciones nativas utilizadas:**
+  - `MongoClient`, `connect()`, `db.collection()`, `updateOne()`, `res.json()`
+
+## Endpoint 26
+- **URL:** `/endpoint26`
+- **Descripción:** Lista todos los ingredientes ordenados alfabéticamente por nombre.
+- **Método HTTP:** GET
+- **Funciones nativas utilizadas:**
+  - `MongoClient`, `connect()`, `db.collection()`, `find()`, `sort()`, `toArray()`, `res.json()`
+
+## Endpoint 27
+- **URL:** `/endpoint27`
+- **Descripción:** Encuentra la hamburguesa más cara en la base de datos.
+- **Método HTTP:** GET
+- **Funciones nativas utilizadas:**
+  - `MongoClient`, `connect()`, `db.collection()`, `find()`, `sort()`, `limit()`, `toArray()`, `res.json()`
+
+
+## Endpoint 28
+  -  **URL:** `/endpoint28`
+  -  **Descripción:** Agrega el ingrediente "Pepinillos" a todas las hamburguesas de la categoría "Clásica".
+  -  **Método HTTP:** GET
+  -  **Funciones nativas utilizadas:**
+       ` MongoClient`,
+       `connect()`,
+        `db.collection()`,
+        `updateMany()`,
+        `res.json()`
+
+## Endpoint 29
+  -  **URL:** `/endpoint29`
+  -  **Descripción:** Elimina todos los chefs especializados en "Cocina Vegetariana" de la base de datos.
+  -  **Método HTTP:** GET
+  -  **Funciones nativas utilizadas:**
+        `MongoClient`,
+        `connect()`,
+        `db.collection()`,
+        `deleteMany()`,
+        `res.json()`
+
+## Endpoint 30
+  -  **URL:** `/endpoint30`
+  -  **Descripción:** Encuentra todas las hamburguesas que contienen exactamente 7 ingredientes.
+  -  **Método HTTP:** GET
+  -  **Funciones nativas utilizadas:**
+        `MongoClient`,
+        `connect()`,
+        `db.collection()`,
+       ` find()`,
+        `toArray()`,
+        `res.json()`
+
+## Endpoint 31
+  -  **URL:** `/endpoint31`
+  -  **Descripción:** Encuentra la hamburguesa gourmet más cara en la base de datos.
+  -  **Método HTTP:** GET
+  -  **Funciones nativas utilizadas:**
+        `MongoClient`,
+        `connect()`,
+        `db.collection()`,
+        `find()`,
+        `sort()`,
+        `limit()`,
+        `toArray()`,
+        `res.json()`
+
+
+## Endpoint 32
+  -  **URL:** `/endpoint32`
+  -  **Descripción:** Realiza una agregación para contar cuántas veces se usa cada ingrediente en las hamburguesas y devuelve el resultado.
+  -  **Método HTTP:** GET
+  -  **Funciones nativas utilizadas:**
+        `MongoClient`,
+        `connect()`,
+        `db.collection()`,
+        `aggregate()`,
+        `$unwind`,
+        `$group`,
+        `toArray()`,
+        `res.json()`
+
+## Endpoint 33
+  -  **URL:** `/endpoint33`
+  -  **Descripción:** Realiza una agregación para contar cuántas hamburguesas ha preparado cada chef y devuelve el resultado.
+  -  **Método HTTP:** GET
+  -  **Funciones nativas utilizadas:**
+        `MongoClient`,
+        `connect()`,
+        `db.collection()`,
+        `aggregate()`,
+        `$group`,
+        `toArray()`,
+        `res.json()`
+
+## Endpoint 34
+  -  **URL:** `/endpoint34`
+  -  **Descripción:** Encuentra la categoría con la menor cantidad de hamburguesas y la devuelve.
+  -  **Método HTTP:** GET
+  -  **Funciones nativas utilizadas:**
+        `MongoClient`,
+        `connect()`,
+        `db.collection()`,
+        `aggregate()`,
+        `$group`,
+        `$sort`,
+        `$limit`,
+        `toArray()`,
+        `res.json()`
+
+## Endpoint 35
+  -  **URL:** `/endpoint35`
+  -  **Descripción:** Calcula el costo total de las hamburguesas preparadas por cada chef y lo devuelve.
+  -  **Método HTTP:** GET
+  -  **Funciones nativas utilizadas:**
+        `MongoClient`,
+        `connect()`,
+        `db.collection()`,
+        `distinct()`,
+        `find()`,
+        `toArray()`,
+        `$nin`,
+        `res.json()`,
+
+# Endpoints de MongoDB
+
+En este archivo se documentan varios endpoints que interactúan con una base de datos MongoDB.
+
+## Endpoint 36
+
+- URL: `/endpoint36`
+- Descripción: Encuentra los ingredientes que no están presentes en ninguna hamburguesa y los devuelve.
+- Método HTTP: GET
+- Funciones nativas utilizadas:
+  - `MongoClient`
+  - `connect()`
+  - `db.collection()`
+  - `distinct()`
+  - `find()`
+  - `$nin`
+  - `toArray()`
+  - `res.json()`
+
+## Endpoint 37
+
+- URL: `/endpoint37`
+- Descripción: Realiza una agregación para obtener el nombre de cada hamburguesa y su descripción de categoría correspondiente.
+- Método HTTP: GET
+- Funciones nativas utilizadas:
+  - `MongoClient`
+  - `connect()`
+  - `db.collection()`
+  - `aggregate()`
+  - `$lookup`
+  - `$project`
+  - `toArray()`
+  - `res.json()`
+
+## Endpoint 38
+
+- URL: `/endpoint38`
+- Descripción: Encuentra el chef que ha utilizado la mayoría de ingredientes en sus hamburguesas.
+- Método HTTP: GET
+- Funciones nativas utilizadas:
+  - `MongoClient`
+  - `connect()`
+  - `db.collection()`
+  - `aggregate()`
+  - `$unwind`
+  - `$group`
+  - `$sort`
+  - `$limit`
+  - `toArray()`
+  - `res.json()`
+
+## Endpoint 39
+
+- URL: `/endpoint39`
+- Descripción: Calcula el precio promedio de las hamburguesas agrupadas por categoría y lo devuelve.
+- Método HTTP: GET
+- Funciones nativas utilizadas:
+  - `MongoClient`
+  - `connect()`
+  - `db.collection()`
+  - `aggregate()`
+  - `$group`
+  - `toArray()`
+  - `res.json()`
+
+## Endpoint 40
+
+- URL: `/endpoint40`
+- Descripción: Encuentra el chef que ha creado la hamburguesa más cara y muestra su nombre junto con el precio de la hamburguesa.
+- Método HTTP: GET
+- Funciones nativas utilizadas:
+  - `MongoClient`
+  - `connect()`
+  - `db.collection()`
+  - `aggregate()`
+  - `$group`
+  - `$max`
+  - `$lookup`
+  - `$project`
+  - `toArray()`
+  - `res.json()`
+
+
+
+
+
+# Operadores y Conceptos MongoDB
+
+En MongoDB, se utilizan varios operadores y conceptos para realizar consultas y actualizaciones en la base de datos. Aquí se describen algunos de los operadores y conceptos más comunes:
+
+## Operadores de Comparación
+
+- `$lt`: Significa "menor que" y se utiliza para comparar valores en consultas.
+- `$lte`: Significa "menor o igual que" y se utiliza para comparar valores en consultas.
+
+## Operadores de Actualización
+
+- `$mul`: Se utiliza para multiplicar valores en un documento.
+- `$push`: Agrega elementos a un array en un documento existente.
+- `$set`: Cambia o actualiza el valor de un campo específico en un documento sin afectar otros campos ni reemplazar el documento completo.
+
+## Funciones de Consulta
+
+- `$limit`: Limita el número de documentos recuperados como resultado de una consulta. Útil cuando se desea recuperar un número específico de documentos.
+- `$not`: Se utiliza para negar una expresión en una consulta.
+- `$in`: Busca coincidencias con uno de los valores proporcionados en un array.
+- `$regex`: Permite buscar documentos cuyo campo cumpla con una expresión regular específica. La opción `i` al final ignora mayúsculas y minúsculas.
+
+## Operadores de Agregación
+
+- `$expr`: Se utiliza para evaluar una condición en una agregación.
+- `$size`: Se utiliza para seleccionar documentos en función del tamaño de un campo de tipo array.
+- `$avg`: Calcula el promedio de un conjunto de valores numéricos.
+
+## Conceptos de Agregación
+
+- `$unwind`: Separa los datos de un array en documentos individuales.
+- `$group`: Agrupa documentos en función de un campo y realiza cálculos en los grupos.
+- `$lookup`: Realiza búsquedas en otra colección y agrega los resultados al documento actual.
+
+## Otros Operadores
+
+- `$ne`: Comprueba que un valor no sea igual a otro valor.
+- `$nin`: Comprueba que un valor no coincida con ninguno de los valores proporcionados en un array.
+- `$addToSet`: Agrega un elemento a un array si ese elemento no existe en el array.
+
+Estos operadores y conceptos son esenciales para realizar consultas y actualizaciones avanzadas en MongoDB. Puedes combinarlos y utilizarlos de diversas formas para obtener los resultados deseados en tu base de datos.
--- backend/Routes/routes.js
+++ backend/Routes/routes.js
@@ -344,18 +344,389 @@ router.get('/endpoint20', async (req, res) => {
 })
 
 
+router.get('/endpoint21', async (req, res) => {
+    try{
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Ingredientes');
+        const result = await collection.find({ precio : { $gte : 2, $lte : 5}}).toArray();
+        res.json(result);
+        client.close();
+    }catch(err){
+        console.log(err);
+    }
+})
+
+
+router.get('/endpoint22', async (req, res) => {
+    try{
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Ingredientes');
+
+
+        const filter = { nombre: 'Pan' };
+        const update = { $set: { descripcion: 'Pan fresco y crujiente' } };
+
+        const result = await collection.updateOne(filter, update);
+
+        res.json(result);
+        client.close();
+    }catch(err){
+        console.log(err);
+    }
+})
+
+
+router.get('/endpoint23', async (req, res) => {
+    try{
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Hamburguesas');
+        const result = await collection.find({ ingredientes : { $in : ['Tomate','Lechuga'] }}).toArray();
+        res.json(result);
+        client.close();
+    }catch(err){
+        console.log(err);
+    }
+})
+
+
+router.get('/endpoint24', async (req, res) => {
+    try{
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Chefs');
+        const result = await collection.find({ nombre : { $ne : 'ChefA' }}).toArray();
+        res.json(result);
+        client.close();
+    }catch(err){
+        console.log(err);
+    }
+})
+
+
+router.get('/endpoint25', async (req, res) => {
+    try{
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Hamburguesas');
+
+
+        const filter = { categoria: 'Gourmet' };
+        const update = { $inc: { precio : 2 } };
+
+        const result = await collection.updateOne(filter, update);
+
+        res.json(result);
+        client.close();
+    }catch(err){
+        console.log(err);
+    }
+})
+
+router.get('/endpoint26', async (req, res) => {
+    try{
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Ingredientes');
+        const result = await collection.find().sort({ nombre : 1}).toArray();
+        res.json(result);
+        client.close();
+    }catch(err){
+        console.log(err);
+    }
+})
+
+
+router.get('/endpoint27', async (req, res) => {
+    try{
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Hamburguesas');
+        const result = await collection.find().sort({ precio : -1}).limit(1).toArray();
+        res.json(result);
+        client.close();
+    }catch(err){
+        console.log(err);
+    }
+})
+
+router.get('/endpoint28', async (req, res) => {
+    try{
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Hamburguesas');
+
+
+        const filter = { categoria: 'Clasica' };
+        const update = { $addToSet: { ingredientes: 'Pepinillos' } };
+
+        const result = await collection.updateMany(filter, update);
+
+        res.json(result);
+        client.close();
+    }catch(err){
+        console.log(err);
+    }
+})
+
+
+router.get('/endpoint29', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Chefs');
+    
+        const result = await collection.deleteMany({ especilaidad : 'Cocina Vegetariana'});
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+router.get('/endpoint30', async (req, res) => {
+    try{
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Hamburguesas');
+        const result = await collection.find({ ingredientes : { $size: 7 }}).toArray();
+        res.json(result);
+        client.close();
+    }catch(err){
+        console.log(err);
+    }
+})
+
+
+router.get('/endpoint31', async (req, res) => {
+    try{
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Hamburguesas');
+
+
+        const filter = { categoria : 'Gourmet' };
+        const orden =  { precio: -1 } ;
+      
+
+        const result = await collection.find(filter).sort(orden).limit(1).toArray();
+
+        res.json(result);
+        client.close();
+    }catch(err){
+        console.log(err);
+    }
+})
+
+
+ 
+
+router.get('/endpoint32', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Hamburguesas');
+        const result = await collection.aggregate([
+            { $unwind: '$ingredientes' }, 
+            { $group: { _id: '$ingredientes', count: { $sum: 1 } } } 
+        ]).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+router.get('/endpoint33', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Hamburguesas');
+        const result = await collection.aggregate([
+            
+            { $group: { _id: '$chef', count: { $sum: 1 } } } 
+        ]).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+router.get('/endpoint34', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Hamburguesas');
+        const result = await collection.aggregate([
+            { $group: { _id: '$categorias', count: { $sum: 1 } } },
+            { $sort: {count: 1} }, 
+            { $limit: 1 }
+        ]).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 
 
 
+router.get('/endpoint35', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Hamburguesas');
+        const result = await collection.aggregate([
+            { $unwind: '$ingredientes' }, 
+            { $lookup: { from: 'ingredientes', localField: 'ingredientes', foreignField: 'nombre', as: 'datos' } },
+            { $group: {_id: '$chef', costoTotal: { $sum: { $sum: '$datos.precio' } } }   }
+        ]).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 
 
 
+router.get('/endpoint36', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const hamburguesas = await db.collection('Hamburguesas').distinct('ingredientes');
+        const result = await db.collection('ingredientes').find({ nombre: { $nin: hamburguesas } }).toArray(); //$nin singinfica que no coincide con ningun valor, en este caso devuelve los ingredientes que no esten en alguna hamburguesa
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 
 
+router.get('/endpoint37', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Hamburguesas');
+        const result = await collection.aggregate([
+            { $lookup: { from: 'Categorias', localField: 'Categorias', foreignField: 'nombre', as: 'CategoriasData' } },
+            { $project: {_id: 0, nombre: 1, 'CategoriasData.descripcion': 1 }   }
+        ]).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 
 
+router.get('/endpoint38', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const collection = db.collection('Hamburguesas');
+        const result = await collection.aggregate([
+             { $unwind: '$ingredientes' },
+             { $group: { _id: '$chef', ingredientesCount: { $sum: 1 } } },
+            { $sort: { ingredientesCount: -1  } }, 
+            { $limit: 1 }
+        ]).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 
+
+router.get('/endpoint39', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const result = await db.collection('Hamburguesas').aggregate([
+            {$group: { _id: '$categoria', precioPromedio: { $avg: '$precio'}}}
+        ]).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+
+router.get('/endpoint40', async (req, res) => {
+    try {
+        const client = new MongoClient(bases);
+        await client.connect();
+        const db = client.db(nombreBase);
+        const result = await db.collection('Hamburguesas').aggregate([
+            {$group: { _id: '$chef', hamburguesaCara: { $max: '$precio'}}},
+            { $lookup: { from: 'chef', localField: '_id', foreignField: 'nombre', as: 'chefData' } },
+            { $project: {_id: 0, 'chefData.nombre': 1, hamburguesaCara: 1 }   }
+        ]).toArray();
+        res.json(result);
+        client.close();
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 
 
 
 module.exports = router;
+
+
+//? $lt es un operador de comparación de mongo que significa less than (menor que).
+//todo: $mul es un operador de actualización en mongo usado para multiplicar
+//?primer argumento vacio del método find ({})  significa que se recuperaran todos los documentos de la colecció
+//todo: $push es un operador de actualización (mongo) hecho para agregar elementos a un array en un documento existente
+//?$set se utiliza para cambiar o actualizar el valor de un campo especifico en un documento sin afectar otros campos ni reemplazar el documento completo
+//todo: $limit es una función de consulta que se utiliza para limitar el número de documentos que se recuperan como resultado de una consulta, en este caso solo queremos recuperar 1 documento
+//?$not se usa para negar la expresion que esta en su interior
+//todo:$in es para buscar coincidencias con uno de los valores proporcionados por el array
+//? $regex permite buscar documentos cuyo campo cumpla con una expresión regular específica (lo que esta dentro de los slash /clasico/). la 'i' al final es para omitir mayusculas o minusculas y que estas no afecten la busqueda
+//todo: $lte evalua si un valor en un campo es menor o igual al valor especificado
+//? $expr se usa para evaluar una condicion
+//todo: $size se utiliza para seleccionar documentos en función del tamaño de un campo de tipo array
+//? el sort ordena los resultados de acuerdo al precio en orden ascendente
+//todo: buscamos ingredientes que su precio sea mayor o igual a 2 y menor o igual a 5, empleando $gte(mayor o igual) y $lte(menor o igual)
+//?utilizamos $set nuevamente para actualizar o cambiar un dato especifico
+//todo:verifica que el nombre del chef sea diferente a ChefA, con $ne
+//? $in comprueba si al menos uno de los valores dichos se encuentra en el array
+//todo: $addToSet es un operador de actualización  que se utiliza para agregar un elemento a un array si ese elemento no existe ya en ese array. En este caso, se agrega pepinillos a los ingredientes de las hamburguesas clasicas
+ //? aggregate() hace agregacion (es un proceso que permite realizar transformaciones y operaciones complejas en los datos antes de devolverlos) dentro de hamburguesas, la agregacion tiene 3 etapas ({})
+//todo: $unwind separa los datos de un array, en este caso deja cada ingrediente individual
+//? $group agrupa los ingredientes (por el id) y posteriormente count cuenta cuantos documentos hay en el grupo y suma 1 por cada uno
+//todo: con $lookup se realiza una busqueda de nombres de ingredientes y las almacena en datos
+//? $nin singinfica que no coincide con ningun valor, en este caso devuelve los ingredientes que no esten en alguna hamburguesa
+//todo: $avg para calcular promedio
+
+
+
+
